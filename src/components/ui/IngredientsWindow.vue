3ddce79771ea20bff834c0d89ec5b0e60d321f11
Marcin Stolarczyk <m.stolvrczyk@gmail.com>
2020-02-17 14:21:21 +0100

    connected app with endpoints

---
+++ src/components/ui/IngredientsWindow.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "IngredientsWindow"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ui/SizeWindow.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "SizeWindow"
+    }
+</script>
+
+<style scoped>
+
+</style>
